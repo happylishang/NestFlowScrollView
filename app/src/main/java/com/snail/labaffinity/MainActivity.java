1f881c69d288249009d4b5ae219d0532c33e21e5
happysnail <happylishang@163.com>
2016-07-14 09:43:14 +0800

    background

--- app/src/main/java/com/snail/labaffinity/MainActivity.java
+++ app/src/main/java/com/snail/labaffinity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.snail.labaffinity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.snail.labaffinity.service.BackGroundService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Intent intent = new Intent(MainActivity.this, BackGroundService.class);
+        startService(intent);
+        
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
