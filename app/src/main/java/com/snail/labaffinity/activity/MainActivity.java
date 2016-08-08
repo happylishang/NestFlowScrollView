d3a26fd53832a651b2a3ccac6171f550b5bfdd75
happysnail <happylishang@163.com>
2016-08-08 16:18:26 +0800

    m

--- app/src/main/java/com/snail/labaffinity/activity/MainActivity.java
+++ app/src/main/java/com/snail/labaffinity/activity/MainActivity.java
@@ -3,15 +3,17 @@ package com.snail.labaffinity.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.snail.labaffinity.R;
 import com.snail.labaffinity.service.BackGroundService;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        ButterKnife.bind(this);
         Intent intent = new Intent(MainActivity.this, BackGroundService.class);
         startService(intent);
 
@@ -29,10 +31,12 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
+    }
+
+    @OnClick(R.id.first) void first(){
+
     }
 
     @Override
