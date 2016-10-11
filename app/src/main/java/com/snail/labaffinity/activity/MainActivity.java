3df2bcf896231b31550e3dfe1beb72c4d943203a
happysnail <happylishang@163.com>
2016-10-11 12:42:34 +0800

    RxJava

--- app/src/main/java/com/snail/labaffinity/activity/MainActivity.java
+++ app/src/main/java/com/snail/labaffinity/activity/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.snail.labaffinity.R;
@@ -14,6 +12,7 @@ import com.snail.labaffinity.service.BackGroundService;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import rx.Observable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,29 +34,12 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @OnClick(R.id.first) void first(){
+    @OnClick(R.id.first)
+    void first() {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void startNextActivity() {
+        Observable
     }
 }
