1e2ba89e85145b64eac25853b2cc7865754c2fec
happysnail <happylishang@163.com>
2016-10-11 13:06:59 +0800

    Rxjava 以后下意识使用

--- app/src/main/java/com/snail/labaffinity/activity/MainActivity.java
+++ app/src/main/java/com/snail/labaffinity/activity/MainActivity.java
@@ -5,20 +5,23 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 
 import com.snail.labaffinity.R;
 import com.snail.labaffinity.service.BackGroundService;
+import com.snail.labaffinity.utils.ToastUtil;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import rx.Observable;
+import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,21 +43,29 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.first)
     void first() {
-        schedule();
+        test();
     }
 
-    private void startNextActivity() {
-    }
-
-
-    private void schedule() {
-        Observable.just(1, 2, 3, 4)
-                .subscribeOn(Schedulers.io()) // 指定 subscribe() 发生在 IO 线程
+    private void test() {
+        Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                //在call方法中执行异步任务
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                count++;
+                subscriber.onNext("成功执行异步任务" + count + "次");
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.io()) // 指定 subscribe() 发生在 IO 线程
                 .observeOn(AndroidSchedulers.mainThread()) // 指定 Subscriber 的回调发生在主线程
-                .subscribe(new Action1<Integer>() {
+                .subscribe(new Action1<String>() {
                     @Override
-                    public void call(Integer number) {
-                        Log.d("lishang", "number:" + number);
+                    public void call(String number) {
+                        ToastUtil.show("number:" + number);
                     }
                 });
     }
