2de5819498c2fdedb1582f6c3f4ff85d567e256b
happysnail <happylishang@163.com>
2016-11-02 10:35:50 +0800

    update

--- app/src/main/java/com/snail/labaffinity/activity/SecondActivity.java
+++ app/src/main/java/com/snail/labaffinity/activity/SecondActivity.java
@@ -19,6 +19,6 @@ public class SecondActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 }
