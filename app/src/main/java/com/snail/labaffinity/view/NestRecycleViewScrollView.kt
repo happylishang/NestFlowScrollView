27e75ebbedf4ef7cff97e8bb3b15cdd2acb64fd2
李尚 <happylishang@163.com>
2023-06-05 16:20:29 +0800

    adsf

--- app/src/main/java/com/snail/labaffinity/view/NestRecycleViewScrollView.kt
+++ app/src/main/java/com/snail/labaffinity/view/NestRecycleViewScrollView.kt
@@ -6,7 +6,10 @@ import android.util.Log
 import android.view.View
 import android.widget.FrameLayout
 import androidx.core.view.NestedScrollingParent3
+import androidx.core.view.NestedScrollingParentHelper
 import androidx.core.view.ViewCompat
+import androidx.core.widget.NestedScrollView
+import androidx.recyclerview.widget.RecyclerView
 
 //垂直滚动的ScrollView嵌套recyvleview
 //FrameLayout对于自定垂直的布局很方便
@@ -14,12 +17,16 @@ class NestRecycleViewScrollView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
-    defStyleRes: Int = 0
+    defStyleRes: Int = 0,
 ) : FrameLayout(context, attrs, defStyleAttr, defStyleRes), NestedScrollingParent3 {
 
+    lateinit var upView: NestedScrollView
+    lateinit var bottomView: RecyclerView
+    lateinit var helper: NestedScrollingParentHelper
+    var maxScrollHeight = 0
     val TAG = "NestRecycleViewScrollView"
     override fun onStartNestedScroll(child: View, target: View, axes: Int, type: Int): Boolean {
-        Log.v(TAG, "onStartNestedScroll " +(axes == ViewCompat.SCROLL_AXIS_VERTICAL))
+        Log.v(TAG, "onStartNestedScroll " + (axes == ViewCompat.SCROLL_AXIS_VERTICAL))
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL
     }
 
@@ -28,7 +35,8 @@ class NestRecycleViewScrollView @JvmOverloads constructor(
     }
 
     override fun onStopNestedScroll(target: View, type: Int) {
-        Log.v(TAG, "onStopNestedScroll")
+        Log.v(TAG, "onStopNestedScroll $target")
+        helper.onStopNestedScroll(target);
     }
 
     override fun onNestedScroll(
@@ -38,9 +46,10 @@ class NestRecycleViewScrollView @JvmOverloads constructor(
         dxUnconsumed: Int,
         dyUnconsumed: Int,
         type: Int,
-        consumed: IntArray
+        consumed: IntArray,
     ) {
-        Log.v(TAG, "onNestedScroll")
+        Log.v(TAG, "onNestedScroll $dxUnconsumed $dyUnconsumed ")
+        scrollBy(0, dyUnconsumed)
     }
 
     override fun onNestedScroll(
@@ -49,14 +58,23 @@ class NestRecycleViewScrollView @JvmOverloads constructor(
         dyConsumed: Int,
         dxUnconsumed: Int,
         dyUnconsumed: Int,
-        type: Int
+        type: Int,
     ) {
-        Log.v(TAG, "onNestedScroll")
 
     }
 
+
     override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
-        Log.v(TAG, "onNestedPreScroll")
+        Log.v(TAG, "onNestedPreScroll$consumed $dy  $target ")
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        upView = getChildAt(0) as NestedScrollView
+        bottomView = getChildAt(1) as RecyclerView
+        helper = NestedScrollingParentHelper(this)
+        if (childCount != 2)
+            throw java.lang.RuntimeException("必须两个View，上面是NestScrollView，下面是RecycleView")
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
@@ -66,10 +84,23 @@ class NestRecycleViewScrollView @JvmOverloads constructor(
             getChildAt(i).layout(l, top, r, bottom)
             top += getChildAt(i).measuredHeight
             bottom += getChildAt(i).measuredHeight
+            maxScrollHeight += getChildAt(i).measuredHeight
         }
+        maxScrollHeight -= measuredHeight
+    }
+
+    override fun onNestedFling(
+        target: View,
+        velocityX: Float,
+        velocityY: Float,
+        consumed: Boolean,
+    ): Boolean {
+        Log.v(TAG, "onNestedFling")
+        return super.onNestedFling(target, velocityX, velocityY, consumed)
     }
 
     override fun onNestedPreFling(target: View, velocityX: Float, velocityY: Float): Boolean {
+        Log.v(TAG, "onNestedPreFling  $velocityY")
         return super.onNestedPreFling(target, velocityX, velocityY)
     }
 }
